fa2563082205a9ef82a176be62cac2555bb7c5a3
Vladimir Kotov <tovkotov@yandex.ru>
2020-07-25 13:46:15 +0300

    get folder size added

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+        String folderPath = "C:" + File.separator
+                + "Users" + File.separator
+                + "admin" + File.separator
+                +"Downloads";
+        File file = new File(folderPath);
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
+    }
+}
